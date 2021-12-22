9dbee835d38bc81f990bb37d5014edac3da813a0
Mykhailo Kholmohorov <mykhailo.kholmohorov@valtech.com>
2021-12-22 13:45:52 +0200

    added movie model

--- YMovies.Database/Models/Movie.cs
+++ YMovies.Database/Models/Movie.cs
@@ -9,9 +9,16 @@ namespace YMovies.Database.Models
     class Movie
     {
         public int MovieId { get; set; }
-        public int ImdbId { get; set; }
+        public string ImdbId { get; set; }
         public string Title { get; set; }
         public string PosterUrl { get; set; }
-        public DateTime 
+        public string Year { get; set; }
+        public int CountryId { get; set; }
+        public int CastId { get; set; }
+        public string Plot { get; set; }
+        public decimal Budget { get; set; }
+        public string BoxOffice { get; set; }
+        public decimal ImdbRating { get; set; }
+        public int statisticId { get; set; }
     }
 }
