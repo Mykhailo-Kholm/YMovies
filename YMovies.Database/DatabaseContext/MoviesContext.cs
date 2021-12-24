200812c986b8495d3af1759cf859d6d24c9dfbbe
Mykhailo Kholmohorov <mykhailo.kholmohorov@valtech.com>
2021-12-24 15:57:19 +0200

    change name

--- YMovies.Database/DatabaseContext/MoviesContext.cs
+++ YMovies.Database/DatabaseContext/MoviesContext.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
 using YMovies.Database.Models;
 
-namespace YMovies.Database.MoviesContext
+namespace YMovies.Database.DatabaseContext
 {
     public class MoviesContext:DbContext
     {
