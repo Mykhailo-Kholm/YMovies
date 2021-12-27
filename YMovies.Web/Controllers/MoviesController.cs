bdc8baee0b8064b4b0b20f277773862e99bb3317
onyshkevych-iryna <iryna.onyshkevych@valtech.com>
2021-12-27 20:17:46 +0200

    Main page and details added

---
+++ YMovies.Web/Controllers/MoviesController.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using YMovies.Web.TempModels;
+using YMovies.Web.ViewModels;
+
+namespace YMovies.Web.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public static List<Country> countries = new List<Country>()
+        {
+            new Country()
+            {
+                Id = 1,
+                Name = "US"
+            },
+            new Country()
+            {
+                Id = 2,
+                Name = "Germany"
+            },
+            new Country()
+            {
+                Id = 3,
+                Name = "Ukraine"
+            }
+        };
+
+        public static List<Movie> movies = new List<Movie>()
+        {
+            new Movie()
+            {
+                Id = 1,
+                Name = "Movie one",
+                Rating = 3,
+                Year = 2021,
+                UsersRating = 1,
+                Description = "descriptionOne",
+                GenresList = new List<string>()
+                {
+                    "FirstGenre",
+                    "SecondGenre"
+                },
+                Type = "Movie",
+                Countries = new List<Country>()
+                {
+                    countries[0],
+
+                },
+
+                Cast = "Actor, Director",
+                Budget = 100
+            },
+            new Movie()
+            {
+                Id = 1,
+                Name = "Movie one",
+                Rating = 3,
+                Year = 2021,
+                UsersRating = 1,
+                Description = "descriptionOne",
+                GenresList = new List<string>()
+                {
+                    "FirstGenre",
+                    "SecondGenre"
+                },
+                Type = "Movie",
+                Countries = new List<Country>()
+                {
+                    countries[0],
+                    countries[1]
+
+                },
+
+                Cast = "Actor, Director",
+                Budget = 100
+            },
+            new Movie()
+            {
+                Id = 3,
+                Name = "Movie one",
+                Rating = 3,
+                Year = 2021,
+                UsersRating = 1,
+                Description = "descriptionOne",
+                GenresList = new List<string>()
+                {
+                    "FirstGenre",
+                    "SecondGenre"
+                },
+                Type = "Movie",
+                Countries = new List<Country>()
+                {
+                    countries[0],
+                    countries[1],
+                    countries[2]
+
+                },
+                Cast = "Actor, Director",
+                Budget = 100
+            },
+            new Movie()
+            {
+                Id = 2,
+                Name = "Movie two",
+                Rating = 2,
+                Year = 1998,
+                UsersRating = 12,
+                Description = "desc2",
+                GenresList = new List<string>()
+                {
+                    "ThirdGenre",
+                    "SecondGenre"
+                },
+                Type = "Serial",
+                Countries = new List<Country>()
+                {
+                    countries[0],
+                    countries[1]
+
+                },
+                Cast = "Actors",
+                Budget = 120
+            },
+            new Movie()
+            {
+                Id = 1,
+                Name = "Movie one",
+                Rating = 3,
+                Year = 2021,
+                UsersRating = 1,
+                Description = "descriptionOne",
+                GenresList = new List<string>()
+                {
+                    "FirstGenre",
+                    "SecondGenre"
+                },
+                Type = "Movie",
+                Countries = new List<Country>()
+                {
+                    countries[0],
+                    countries[1]
+
+                },
+                Cast = "Actor, Director",
+                Budget = 100
+            },
+            new Movie()
+            {
+                Id = 1,
+                Name = "Movie one",
+                Rating = 3,
+                Year = 2021,
+                UsersRating = 1,
+                Description = "descriptionOne",
+                GenresList = new List<string>()
+                {
+                    "FirstGenre",
+                    "SecondGenre"
+                },
+                Type = "Movie",
+                Countries = new List<Country>()
+                {
+                    countries[0],
+                    countries[1]
+
+                },
+                Cast = "Actor, Director",
+                Budget = 100
+            },
+            new Movie()
+            {
+                Id = 2,
+                Name = "Movie two",
+                Rating = 2,
+                Year = 1998,
+                UsersRating = 12,
+                Description = "desc2",
+                GenresList = new List<string>()
+                {
+                    "ThirdGenre",
+                    "SecondGenre"
+                },
+                Type = "Serial",
+                Countries = new List<Country>()
+                {
+                    countries[0],
+                    countries[1]
+
+                },
+                Cast = "Actors",
+                Budget = 120
+            }
+        };
+
+        [HttpGet]
+        public ActionResult Index(int? page)
+        {
+            var pageSize = 10;
+            var pageNumber = page ?? 1;
+            var countryMovieViewModel = new CountryMovieViewModel()
+            {
+                MoviePageList = movies.ToPagedList(pageNumber, pageSize),
+                Movies = movies
+            };
+            return View(countryMovieViewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Movie movie = movies.FirstOrDefault(m => m.Id == id);
+            return View(movie);
+        }
+    }
+}
---
+++ YMovies.Web/TempModels/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YMovies.Web.TempModels
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Rating { get; set; }
+        public int UsersRating { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public ICollection<Country> Countries { get; set; }
+        //public Movie()
+        //{
+        //    Countries = new List<Country>();
+        //}
+        public int Year { get; set; }
+        public List<string> GenresList { get; set; }
+        public string Cast { get; set; }
+        public decimal Budget { get; set; }
+    }
+}
