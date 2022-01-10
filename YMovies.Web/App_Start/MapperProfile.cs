69dad7700cb9a9e812bda39fc55f73206f4e3f46
Mykhailo Kholmohorov <mykhailo.kholmohorov@valtech.com>
2022-01-10 21:43:14 +0200

    merged

--- YMovies.Web/App_Start/MapperProfile.cs
+++ YMovies.Web/App_Start/MapperProfile.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Ymovies.Identity.BLL.DTO;
+using YMovies.MovieDbService.DTOs;
 using YMovies.MovieDbService.Models;
 using YMovies.Web.Dtos;
 using YMovies.Web.DTOs;
@@ -21,6 +22,21 @@ namespace YMovies.Web.App_Start
             CreateMap<Genre, GenreWebDto>().ReverseMap();
             CreateMap<Season, SeasonWebDto>().ReverseMap();
             CreateMap<Media, SeriesWebDto>().ReverseMap();
+
+            CreateMap<Cast, CastDto>().ReverseMap();
+            CreateMap<Country, CountryDto>().ReverseMap();
+            CreateMap<Genre, GenreDto>().ReverseMap();
+            CreateMap<Season, SeasonDto>()
+                .ForMember(d => d.CurrentSeries, opt => opt.MapFrom(m => m.CurrentSeries))
+                .ReverseMap();
+            CreateMap<Type, TypeDto>().ReverseMap();
+            CreateMap<Media, MediaDto>()
+                .ForMember(d => d.Cast, opt => opt.MapFrom(m => m.Cast))
+                .ForMember(d => d.Countries, opt => opt.MapFrom(m => m.Countries))
+                .ForMember(d => d.Genres, opt => opt.MapFrom(m => m.Genres))
+                .ForMember(d => d.Seasons, opt => opt.MapFrom(m => m.Seasons))
+                .ForMember(d => d.Type, opt => opt.MapFrom(m => m.Type.Name))
+                .ReverseMap();
         }
     }
 }
