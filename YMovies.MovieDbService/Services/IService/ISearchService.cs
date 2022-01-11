3e3789652d159b51caa6bdfc0c5d3f8196399ce4
onyshkevych-iryna <iryna.onyshkevych@valtech.com>
2022-01-11 19:53:30 +0200

    Searching added

---
+++ YMovies.MovieDbService/Services/IService/ISearchService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YMovies.MovieDbService.DTOs;
+
+namespace YMovies.MovieDbService.Services.IService
+{
+    public interface ISearchService
+    {
+        MediaDto GetItem(string id);
+        List<MediaDto> GetOneHundredMediaRandom();
+        List<MediaDto> GetMostLiked();
+        List<MediaDto> GetMediaByTitle(string title);
+        List<MediaDto> GetMediaByParams(string[] genre, string[] country, string year, string type);
+    }
+}
