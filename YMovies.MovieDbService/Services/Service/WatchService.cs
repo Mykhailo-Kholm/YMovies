d0187e1fc342fe641aa6940eaf1d59cfc1941fa5
Mykhailo Kholmohorov <mykhailo.kholmohorov@valtech.com>
2022-01-11 21:07:07 +0200

    upgrate likes watch functionality

--- YMovies.MovieDbService/Services/Service/WatchService.cs
+++ YMovies.MovieDbService/Services/Service/WatchService.cs
@@ -7,20 +7,26 @@ namespace YMovies.MovieDbService.Services.Service
 {
     public class WatchService
     {
-        private readonly IRepository<Media> _mediaRepository;
-        private readonly IRepository<User> _userRepository;
+        private readonly MovieRepository _mediaRepository;
+        private readonly UserRepository _userRepository;
 
         public WatchService(MoviesContext context)
         {
             _mediaRepository = new MovieRepository(context);
             _userRepository = new UserRepository(context);
         }
-        public void WatchedMediaByUser(int userId, int mediaId)
+        public bool WatchedMediaByUser(string userId, int mediaId)
         {
             var user = _userRepository.GetItem(userId);
             var media = _mediaRepository.GetItem(mediaId);
-            user.WatchedMedias.Add(media);
-            _userRepository.UpdateItem(user);
+            if (!user.WatchedMedias.Contains(media))
+            {
+                user.WatchedMedias.Add(media);
+                _userRepository.UpdateItem(user);
+                return true;
+            }
+
+            return false;
         }
     }
 }
