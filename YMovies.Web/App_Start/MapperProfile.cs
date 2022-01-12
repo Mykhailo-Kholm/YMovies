f9535737fcbf0d883da7dbd2a89f1e632a6bc77c
V1adys1avchik <vladyslav.avramchuk@valtech.com>
2022-01-12 02:51:48 +0200

    Fix Mapper maping formember

--- YMovies.Web/App_Start/MapperProfile.cs
+++ YMovies.Web/App_Start/MapperProfile.cs
@@ -25,10 +25,18 @@ namespace YMovies.Web.App_Start
             CreateMap<MostPopularDataDetail, IndexMediaViewModel>()
                  .ForMember(dest => dest.ImdbRating,
                      opt => opt.MapFrom(src => TypeConverter.ToDecimal(src.IMDbRating)))
+                 .ForMember(dest => dest.ImdbId,
+                     opt => opt.MapFrom(src => src.Id))
+                 .ForMember(dest => dest.PosterUrl,
+                     opt => opt.MapFrom(src => src.Image))
                  .ReverseMap();
             CreateMap<Top250DataDetail, IndexMediaViewModel>()
                 .ForMember(dest => dest.ImdbRating,
-                    opt => opt.MapFrom(src => TypeConverter.ToDecimal(src.IMDbRating)));
+                    opt => opt.MapFrom(src => TypeConverter.ToDecimal(src.IMDbRating)))
+                .ForMember(dest => dest.ImdbId,
+                    opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest =>dest.PosterUrl,
+                    opt=>opt.MapFrom(src => src.Image));
             CreateMap<Top250DataDetail, MediaDto>()
                .ForMember(dest => dest.ImdbRating,
                    opt => opt.MapFrom(src => TypeConverter.ToDecimal(src.IMDbRating)));
