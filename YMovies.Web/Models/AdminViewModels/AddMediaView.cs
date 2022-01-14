ebc12bcfc34bb7cc737ca65235748409b8d1aa0f
V1adys1avchik <vladyslav.avramchuk@valtech.com>
2022-01-14 23:47:31 +0200

    Add view|model|method for adding media from imdb id

---
+++ YMovies.Web/Models/AdminViewModels/AddMediaView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace YMovies.Web.Models.AdminViewModels
+{
+    public class AddMediaView
+    {
+        [Required]
+        [RegularExpression(@"^tt[0-9]{6,8}$")]
+        public string ImdbId { get; set; }
+    }
+}
