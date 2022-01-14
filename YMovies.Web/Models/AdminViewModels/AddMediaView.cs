cc71eadc7d35f151b9cfa451a38b6e456b0d7fab
V1adys1avchik <vladyslav.avramchuk@valtech.com>
2022-01-14 23:52:41 +0200

    fix-regex error mess.

--- YMovies.Web/Models/AdminViewModels/AddMediaView.cs
+++ YMovies.Web/Models/AdminViewModels/AddMediaView.cs
@@ -9,7 +9,7 @@ namespace YMovies.Web.Models.AdminViewModels
     public class AddMediaView
     {
         [Required]
-        [RegularExpression(@"^tt[0-9]{6,8}$")]
+        [RegularExpression(@"^tt[0-9]{6,8}$", ErrorMessage = "input id like : tt(100001)-(10000001)")]
         public string ImdbId { get; set; }
     }
 }
